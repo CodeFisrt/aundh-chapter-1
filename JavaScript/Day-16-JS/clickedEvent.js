009c5d99ddbcea96f9e72fe856c0f2847a89ea88
sagar kolhe <sagarkolhe055@gmail.com>
2025-08-05 12:09:05 +0530

    Dom and its Manupaltion

---
+++ JavaScript/Day-16-JS/clickedEvent.js
@@ -0,0 +1,9 @@
+function demoClike(){
+    textColor()
+    console.log("button cliked")
+}
+
+function textColor(){
+    var demo = document.getElementById("demo");
+    demo.style.color = "red"
+}
---
+++ JavaScript/Day-16-JS/domStudy.js
@@ -0,0 +1,23 @@
+//  function dom(){
+//     setTimeout ( () =>{
+//             var demo = document.getElementById("dom1");
+//             demo.style.backgroundColor = "red";
+//     },1000 )
+//         }
+//         dom()
+
+        function dom1(){
+            setTimeout(()=>{
+                var demo1 = document.getElementsByClassName('contanier1')[0];
+                demo1.style.backgroundColor = "red"
+            }, 500)
+        }
+        dom1()
+
+
+
+// function demo(){
+//     var demo = document.getElementById("dom1")
+//     demo.style.color = "red"
+// }
+// demo()
