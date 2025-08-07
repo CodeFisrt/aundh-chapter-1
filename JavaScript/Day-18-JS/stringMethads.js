13bd959c42f3e0fe8e24de8bc71e174494899cc6
sagar kolhe <sagarkolhe055@gmail.com>
2025-08-07 15:38:41 +0530

    String methods practice using trim(), concat(),split()....

---
+++ JavaScript/Day-18-JS/stringMethads.js
@@ -0,0 +1,60 @@
+var str = "Code Frist technology";
+// console.log(str)
+
+// split()
+// console.log(str.split(""));
+
+// length
+// console.log(str.length);
+
+// charAt(20)
+// console.log(str.charAt(20));
+
+// at()
+// console.log(str.at(19))
+
+// charCodeAt(1)
+// console.log(str.charCodeAt(1));
+
+// concat()
+// var text1 = "Code";
+// var text2 = "Frist";
+// console.log(text1.concat(text2))
+// var text3 = text1.concat(text2) 
+// console.log(text3)
+
+// var text = "Apple, Banana, Kiwi";
+// console.log(text.split(""))
+// console.log(text.length);
+// var method = text.slice(7,12);
+// console.log(method);
+
+// toUpperCase and toLowerCase
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+
+// trim()
+// var str = ' Jay Maharashtr ';
+// console.log(str.trimStart());
+// console.log(str.trimEnd());
+// var str1 = str.trim();
+// console.log( str," ", str.length)
+// console.log( str1," ", str1.length)
+// console.log(str.trim());
+// console.log(str.length)
+
+// var str3 = "Code Frist 083kmckw].s's]d/"
+// console.log(str3.tr)
+
+
+// var num = 2323;
+// var num1 = num.toString;
+// console.log(num1.repeat(2 ));
+
+// repeat()
+// var str4 = "Hello "
+// console.log(str4.repeat(2))
+
+// var text4 = "Code Frist"
+// var result = text4.replace("Code Frist" , "CodeFrist")
+// console.log(result)
