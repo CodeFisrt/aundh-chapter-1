5aece0aec67a6595405cd9f3d51a298c84f0a281
sagar kolhe <sagarkolhe055@gmail.com>
2025-08-12 10:58:34 +0530

    Array methods practiec filter,map method use

---
+++ JavaScript/Day-20-Array/filter.js
@@ -0,0 +1,18 @@
+var arr = [18,29,20,10,26,17];
+function demo(age){
+    return age >= 18;
+}
+var a = arr.filter(demo);
+console.log(a);
+
+var b = 12343;
+var number = b.toString();
+// console.log(number)
+// console.log(b)
+var res = number.split("").reverse().join("")
+return res == b;
+// console.log(res)
+
+// var str = "sagar";
+// var str2 = str.split("").reverse().join("");
+// console.log(str2)
---
+++ JavaScript/Day-20-Array/map-Method.js
@@ -0,0 +1,25 @@
+var arr = [4,9,16,25,100,36];
+
+// var a = arr.map(x => x*2);
+// console.log(a);
+
+// var a = arr.map(v => v/2);
+// console.log(a)
+
+// var a = arr.map(x => Math.sqrt(x));
+// console.log(a)
+console.log(arr)
+var res = arr.map(function demo(   index , val){
+    return {index : index , value : val*val}
+
+})
+// console.log(res);
+
+var myaar = ['CodeFrist']
+console.log(myaar)
+var a = myaar
+
+var newarray = Array.from(
+    "CodeFrist"
+);
+console.log(newarray)
