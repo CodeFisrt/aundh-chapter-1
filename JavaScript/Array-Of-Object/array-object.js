20a257cd8f13708908f444a7a2427042bb93ec2a
sagar kolhe <sagarkolhe055@gmail.com>
2025-08-15 09:57:16 +0530

    ArrayOfObject codes

---
+++ JavaScript/Array-Of-Object/array-object.js
@@ -0,0 +1,34 @@
+var a = [
+    {
+        a:"2",
+        b:"3",
+        c:"4",
+        d:"5"
+    }
+]
+
+console.log(a)
+
+var arrObj = [
+{
+    name:"sagar",
+    age: 24
+},
+{
+    name:"Sham",
+    age : 23
+},
+{
+    name:"Ram",
+    age:24
+}
+]
+// console.log(arrObj);
+// arrObj.forEach(function(item){
+//     console.log(item);
+// })
+
+arrObj.map(function (element){
+    // return element
+    console.log(arrObj)
+})
